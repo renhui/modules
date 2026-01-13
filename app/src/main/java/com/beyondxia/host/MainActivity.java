d6c55b2b094dafb8789a1b106ce0a1f8b9bfbc16
灰色飘零 <1418757529@qq.com>
2026-01-13 09:08:19 +0800

    提交

--- app/src/main/java/com/beyondxia/host/MainActivity.java
+++ app/src/main/java/com/beyondxia/host/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.beyondxia.bussiness1.export.ILogin;
 import com.beyondxia.bussiness1.export.LoginService;
+import com.beyondxia.message.export.IMessage;
 import com.beyondxia.message.export.MessageService;
 import com.beyondxia.modules.BCDictionary;
 import com.beyondxia.modules.R;
@@ -24,22 +26,47 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_business1_call:
-                String username = LoginService.get().getUserName();
-                Toast.makeText(this,username,Toast.LENGTH_SHORT).show();
+                ILogin loginService = LoginService.get();
+                if (loginService != null) {
+                    String username = loginService.getUserName();
+                    Toast.makeText(this, username, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "LoginService未初始化，请检查服务注册", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.bt_business1_call2:
-                boolean success = LoginService.get().doLogin(this,"chenwei","xiaxiaojun");
-                Toast.makeText(this,success?"login success":"login failed",Toast.LENGTH_SHORT).show();
+                ILogin loginService2 = LoginService.get();
+                if (loginService2 != null) {
+                    boolean success = loginService2.doLogin(this, "chenwei", "xiaxiaojun");
+                    Toast.makeText(this, success ? "login success" : "login failed", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "LoginService未初始化，请检查服务注册", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.bt_business1_call3:
-                BCDictionary dictionary = LoginService.get().getUserInfo();
-                Toast.makeText(this,dictionary.toJson(),Toast.LENGTH_SHORT).show();
+                ILogin loginService3 = LoginService.get();
+                if (loginService3 != null) {
+                    BCDictionary dictionary = loginService3.getUserInfo();
+                    Toast.makeText(this, dictionary.toJson(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "LoginService未初始化，请检查服务注册", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.bt_business1_nav:
-                LoginService.get().nav2LoginActivity(this);
+                ILogin loginService4 = LoginService.get();
+                if (loginService4 != null) {
+                    loginService4.nav2LoginActivity(this);
+                } else {
+                    Toast.makeText(this, "LoginService未初始化，请检查服务注册", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.bt_business2_nav:
-                MessageService.get().nav2B2Activity(this);
+                IMessage messageService = MessageService.get();
+                if (messageService != null) {
+                    messageService.nav2B2Activity(this);
+                } else {
+                    Toast.makeText(this, "MessageService未初始化，请检查服务注册", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.bt_submodule_view:
                 startActivity(new Intent(this, ShowViewActivity.class));
